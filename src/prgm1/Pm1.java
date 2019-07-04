d473f27dd8735d49741a1a23acc6c6996c8cf632
Aditya Pasuparthi <aditya.sainaveen@gmail.com>
2019-07-04 12:10:02 +0530

    Commit for second message

--- src/prgm1/Pm1.java
+++ src/prgm1/Pm1.java
@@ -2,9 +2,8 @@ package prgm1;
 
 public class Pm1 
 {
-	public static void main(String args)
+	public static void main(String[] args) 
 	{
-		System.out.print("Aditya");
+		System.out.println("Aditya");
 	}
-
 }
---
+++ src/prgm1/Pm3.java
@@ -0,0 +1,14 @@
+package prgm1;
+
+public class Pm3 
+{
+	public static void main(String args[])
+	{
+		int a=5;
+		int b=10;
+		int c = a%b;
+		
+		System.out.print(c+" is a number");
+	}
+
+}
