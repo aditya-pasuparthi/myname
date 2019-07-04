17c972abdff9737a5c4a085825a713cd1fa0d74a
Aditya Pasuparthi <aditya.sainaveen@gmail.com>
2019-07-04 12:23:05 +0530

    five

--- src/prgm1/Pm3.java
+++ src/prgm1/Pm3.java
@@ -4,6 +4,9 @@ public class Pm3
 {
 		public static void main(String[] args) 
 		{
-			System.out.println("Aditya");
+			int a = 5;
+			int b= 10;
+			int c = a+b;
+			System.out.println(c+" Aditya");
 		}
 }
